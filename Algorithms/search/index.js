57787bc5dd780f1c0b417f49088cc8da36b1d7b9
Miguel Ribeiro <miguel.ribeiro@mindera.com>
2021-01-08 18:12:03 +0000

    chore: add binary search

--- Algorithms/search/index.js
+++ Algorithms/search/index.js
@@ -15,6 +15,28 @@ function linearSearch(arr, target) {
   return -1;
 }
 
+/**
+ * Checks the array by dividing the array in half chuncks.
+ * Because we are halving the array this only works on sorted arrays.
+ */
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== target && start <= end) {
+    if (target < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+
+  return arr[middle] === target ? middle : -1;
+}
+
 module.exports = {
   linearSearch: linearSearch,
+  binarySearch: binarySearch,
 };
--- Algorithms/search/search.spec.js
+++ Algorithms/search/search.spec.js
@@ -1,4 +1,4 @@
-const { linearSearch } = require("./index");
+const { linearSearch, binarySearch } = require("./index");
 
 describe("::linearSearch", () => {
   describe("given the array", () => {
@@ -24,6 +24,7 @@ describe("::linearSearch", () => {
   describe("given the array", () => {
     describe("and the target value", () => {
       let arr = [10, 15, 20, 25, 30];
+
       it("should return 1", () => {
         expect(linearSearch(arr, 15)).toBe(1);
       });
@@ -43,6 +44,7 @@ describe("::linearSearch", () => {
   describe("given the array", () => {
     describe("and the target value", () => {
       let arr = [100];
+
       it("should return 0", () => {
         expect(linearSearch(arr, 100)).toBe(0);
       });
@@ -52,8 +54,138 @@ describe("::linearSearch", () => {
   describe("given the array", () => {
     describe("and the target value", () => {
       let arr = [100];
+
       it("should return -1", () => {
         expect(linearSearch(arr, 200)).toBe(-1);
+      });
+    });
+  });
+});
+
+describe("::binarySearch", () => {
+  describe("given the array", () => {
+    describe("and the target value", () => {
+      let arr = [1, 2, 3, 4, 5];
+
+      it("should return 1", () => {
+        expect(binarySearch(arr, 2)).toBe(1);
+      });
+    });
+  });
+
+  describe("given the array", () => {
+    describe("and the target value", () => {
+      let arr = [1, 2, 3, 4, 5];
+
+      it("should return 2", () => {
+        expect(binarySearch(arr, 3)).toBe(2);
+      });
+    });
+  });
+
+  describe("given the array", () => {
+    describe("and the target value", () => {
+      let arr = [1, 2, 3, 4, 5];
+
+      it("should return 4", () => {
+        expect(binarySearch(arr, 5)).toBe(4);
+      });
+    });
+  });
+
+  describe("given the array", () => {
+    describe("and the target value", () => {
+      let arr = [1, 2, 3, 4, 5];
+
+      it("should return -1", () => {
+        expect(binarySearch(arr, 6)).toBe(-1);
+      });
+    });
+  });
+
+  describe("given the array", () => {
+    describe("and the target value", () => {
+      let arr = [5,
+        6,
+        10,
+        13,
+        14,
+        18,
+        30,
+        34,
+        35,
+        37,
+        40,
+        44,
+        64,
+        79,
+        84,
+        86,
+        95,
+        96,
+        98,
+        99,];
+
+      it("should return 2", () => {
+        expect(binarySearch(arr, 10)).toBe(2);
+      });
+    });
+  });
+
+  describe("given the array", () => {
+    describe("and the target value", () => {
+      let arr = [5,
+        6,
+        10,
+        13,
+        14,
+        18,
+        30,
+        34,
+        35,
+        37,
+        40,
+        44,
+        64,
+        79,
+        84,
+        86,
+        95,
+        96,
+        98,
+        99,];
+
+      it("should return 16", () => {
+        expect(binarySearch(arr, 95)).toBe(16);
+      });
+    });
+  });
+
+  describe("given the array", () => {
+    describe("and the target value", () => {
+      let arr = [5,
+        6,
+        10,
+        13,
+        14,
+        18,
+        30,
+        34,
+        35,
+        37,
+        40,
+        44,
+        64,
+        79,
+        84,
+        86,
+        95,
+        96,
+        98,
+        99,];
+
+      it("should return -1", () => {
+        expect(binarySearch(arr, 100)).toBe(-1);
       });
     });
   });
